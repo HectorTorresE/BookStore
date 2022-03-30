31f143f1ea01559f61d6152f576ab8bdad64e046
HectorTorresE <hectorjte@gmail.com>
2022-03-30 15:50:15 -0600

    Change function to arrow functions

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,52 +1,35 @@
 const ADD = 'book-store/books/ADD ';
 const REMOVE = 'book-store/books/REMOVE';
+const INITIAL_STATE = [];
 
-const initialState = {
-  books: [
-    {
-      id: '1',
-      author: 'Gayle Laakmann McDowell',
-      title: 'Cracking the Coding Interview',
-    },
-    {
-      id: '2',
-      author: 'Robert Greene',
-      title: 'The 48 Laws of Power',
-    },
-  ],
+
+export const add = (book) => ({
+  type: ADD,
+  obj: book,
+});
+
+export const remove = (id) => ({
+  type: REMOVE,
+  obj: id,
+});
+
+const reducer = (state = INITIAL_STATE, action) => {
+  if (action.type === ADD) {
+    return {
+      books: [
+        ...state.books,
+        {
+          id: action.obj.id,
+          title: action.obj.title,
+          author: action.obj.author,
+        },
+      ],
+    };
+  }
+  if (action.type === REMOVE) {
+    return state.filter((item) => item.id !== action.obj);
+  }
+  return state;
 };
 
-export function add(book) {
-  return {
-    type: ADD,
-    obj: book,
-  };
-}
-
-export function remove(id) {
-  return {
-    type: REMOVE,
-    obj: id,
-  };
-}
-
-export default function reducer(state = initialState, action) {
-  switch (action.type) {
-    case ADD:
-      return {
-        books: [
-          ...state.books,
-          {
-            id: action.obj.id,
-            title: action.obj.title,
-            author: action.obj.author,
-          },
-        ],
-      };
-    case REMOVE:
-      return {
-        books: state.books.filter((book) => (book.id !== action.obj.id)),
-      };
-    default: return state;
-  }
-}
+export default reducer;
--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,17 +1,17 @@
 const STATUS = 'book-store//categories/STATUS';
 const INITIAL_STATE = [];
 
-export function checkStatus(id) {
-  return {
-    type: STATUS,
-    payload: id,
-  };
-}
+export const checkStatus = (id) => ({
+  type: STATUS,
+  obj: id,
+});
 
-export default function reducer(state = INITIAL_STATE, action) {
+const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case STATUS:
       return { status: 'Pending' };
     default: return state;
   }
-}
+};
+
+export default reducer;
